b2cebb4e4e2140fd9224eb0cf92db8d852071312
Nandana Mukherjee <nandana.mukh14@gmail.com>
2026-05-18 03:09:05 +0530

    updated train.py model training logic

--- ml-model/train.py
+++ ml-model/train.py
@@ -7,7 +7,8 @@ import os
 
 # ================= CONNECT MONGODB ATLAS =================
 
-MONGO_URI = "mongodb+srv://nandanamukh14_db_user:Pnu%402003@cluster0.qaxu26u.mongodb.net/BankLoan?retryWrites=true&w=majority"
+
+MONGO_URI = "mongodb://nandanamukh14_db_user:Pnu%402003@ac-ffppp0r-shard-00-00.qaxu26u.mongodb.net:27017,ac-ffppp0r-shard-00-01.qaxu26u.mongodb.net:27017,ac-ffppp0r-shard-00-02.qaxu26u.mongodb.net:27017/BankLoan?replicaSet=atlas-13uzun-shard-0&ssl=true&authSource=admin"
 
 client = MongoClient(MONGO_URI)
 
